0cc138c668bda36f1d085bed9bb3bde98e984fb1
amr elhewy <amrelhewi@gmail.com>
2020-05-16 16:41:36 +0200

    Login v1.0.0

--- server/app.js
+++ server/app.js
@@ -13,6 +13,8 @@ mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:
 })
 const PORT=process.env.PORT || 5000;
 app.use(cors({credentials:true, origin:"http://localhost:8080"}));
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug')
 app.use(express.json());
 app.use(cookieparser());
 app.use('/account',require('./routes/account'));
--- server/routes/account.js
+++ server/routes/account.js
@@ -5,6 +5,7 @@ const nodemailer = require("nodemailer");
 const smtpTransport = require("nodemailer-smtp-transport");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const Joi = require('@hapi/joi');
 const multer = require("multer");
 const cloudinary = require("cloudinary");
 const cloudinaryStorage = require("multer-storage-cloudinary");
@@ -20,19 +21,20 @@ const storage = cloudinaryStorage({
   transformation: [{ width: 500, height: 500, crop: "limit" }]
 });
 const upload = multer({ storage: storage });
+const transporter = nodemailer.createTransport(
+  smtpTransport({
+    service: "SendGrid",
+    auth: {
+      user: process.env.sendgrid_id,
+      pass: process.env.sendgrid_pw
+    }
+  })
+);
 router.post("/register", async (req, res) => {
   const user = req.body;
   let ExistingUser = await User.findOne({ email: user.email });
   if (ExistingUser) return res.status(400).send("User already exists");
-  let transporter = nodemailer.createTransport(
-    smtpTransport({
-      service: "SendGrid",
-      auth: {
-        user: process.env.sendgrid_id,
-        pass: process.env.sendgrid_pw
-      }
-    })
-  );
+ 
   const token = jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: "3m" });
   let link = `<a href="http://localhost:5000/account/register/${token}">Confirm</a>`;
   let info = await transporter.sendMail({
@@ -109,21 +111,12 @@ router.put("/change-pw", async (req, res) => {
   const email = req.query.email;
   const existinguser = await User.findOne({ email: email });
   if (!existinguser) return res.status(404).send("Email not found");
-  let transporter = nodemailer.createTransport(
-    smtpTransport({
-      service: "Gmail",
-      auth: {
-        user: process.env.gmail_id,
-        pass: process.env.gmail_pw
-      }
-    })
-  );
   const token = jwt.sign({ email: email }, process.env.TOKEN_SECRET, {
     expiresIn: "3m"
   });
   let link = `<a href="http://localhost:5000/account/change-pw/${token}">Change Password</a>`;
   let info = await transporter.sendMail({
-    from: "noreply@domain.com", // sender address
+    from: "noreplyhewy@gmail.com", // sender address
     to: email, // list of receivers
     subject: "Password Reset", // Subject line
     html: `<p>Please click on the link below to reset your password</p> <br> ${link}` // html body
@@ -134,8 +127,39 @@ router.get("/change-pw/:token", async (req, res) => {
   const token = req.params.token;
   try {
     const validate = jwt.verify(token, process.env.TOKEN_SECRET);
+    res.render('resetpw',{email:validate.email});
   } catch (err) {
     res.status(400).send("<h4>This link has expired</h4>");
   }
+});
+
+router.post("/changed-pw/:email",express.urlencoded({extended:true}),async(req,res)=>{
+  const email=req.params.email;
+  const body=req.body;
+ const schema= Joi.object({
+     password: Joi.string().pattern(new RegExp('^(?=.*?[A-Z])(?=.*?[a-z]).{5,}$')),
+     confirmpassword: Joi.ref('password')
+  });
+  try {
+    const value = await schema.validateAsync(body);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(value.password, salt);
+    await User.findOneAndUpdate({email:email},{$set:{password:hashedPassword}});
+    res.redirect('http://localhost:8080/');
+}
+catch (err) {res.render('resetpw',{error:'Password must have at least one upper case, one lower case and must be at least 5 characters'}); }
+
+});
+router.post('/changed-pass',async(req,res)=>{
+const {email,password,oldpw}=req.body;
+const ExistingUser=await User.findOne({email:email});
+const comparepass=await bcrypt.compare(oldpw,ExistingUser.password);
+if(!comparepass) return res.status(401).send('Incorrect old password');
+const salt=await bcrypt.genSalt(10);
+const hashedPassword=await bcrypt.hash(password,salt);
+ExistingUser.password=hashedPassword;
+await ExistingUser.save();
+res.status(200).send('Password Changed Successfully');
+
 });
 module.exports = router;
